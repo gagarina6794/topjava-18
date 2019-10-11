7e5464289fa98f65688be3d7cb4379987dea14ab
irhry@smile.fr <iryna6794@gmail.com>
2019-10-11 17:24:44 +0300

    release that the Meal must belong to user in MealRepository, make refactoring in Repository architecture

---
+++ src/main/java/ru/javawebinar/topjava/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.repository;
+
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    User save(User meal);
+
+    boolean delete(int id);
+
+    User get(int id);
+
+    List<User> getAll();
+
+    User getByEmail(String email);
+}
