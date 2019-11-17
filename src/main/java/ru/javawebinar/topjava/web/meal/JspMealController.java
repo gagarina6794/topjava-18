3c62e0c9458c1e47c56376db9583d7d84f0699b9
iryna6794@gmail.com <hryzantema6794>
2019-11-17 13:54:00 +0200

    modify JDBC User Repository to work with roles, fix not required left join fot roles in JPA repositories, add new test notFoundByEmail, add user with two roles in tests

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -41,7 +41,7 @@ public class JspMealController extends AbstractMealController {
         return "redirect:" + PATH;
     }
 
-    @GetMapping("/mealForm")
+    @GetMapping("/update")
     public String getForUpdate(HttpServletRequest request) {
         int id = getId(request);
         request.setAttribute("meal", super.get(id));
